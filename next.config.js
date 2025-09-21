54aba1191d9eb23cea3482441972510644efc458
ashlyn <68078477+4shlyn@users.noreply.github.com>
2025-09-20 22:50:43 -0400

    debugging

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.ashlynl.com' }],
+        destination: 'https://ashlynl.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig;
